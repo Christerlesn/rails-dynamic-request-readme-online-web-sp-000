954e81be67d3947337a12b70be6138c55c07d55c
Christerle Nounoute <christerlesn@gmail.com>
2021-04-12 04:33:25 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,7 +11,7 @@ describe 'navigate' do
   end
 
   it 'shows the title on the show page' do
-    visit "/posts#{@post.id}"
+    visit "/posts/#{@post.id}"
     expect(page).to have_css("h1", text:"My Post")
   end
 end
